a42093f462d65242dfe4c7e7537129c201f54db8
kondyrev <kondyrev@gmail.com>
2025-06-23 16:47:28 +0300

    первый комит

---
+++ cartridges/forms.py
@@ -0,0 +1,53 @@
+from django import forms
+from .models import Department, Printer, Cartridge, CartridgeMinStock, CartridgeInventory, UsageRecord
+
+
+class DepartmentForm(forms.ModelForm):
+    class Meta:
+        model = Department
+        fields = ['name']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+
+
+class PrinterForm(forms.ModelForm):
+    class Meta:
+        model = Printer
+        fields = ['name', 'department', 'location']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'department': forms.Select(attrs={'class': 'form-select'}),
+            'location': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+
+
+class CartridgeForm(forms.ModelForm):
+    class Meta:
+        model = Cartridge
+        fields = ['name', 'article']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'article': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+
+
+class CartridgeMinStockForm(forms.ModelForm):
+    class Meta:
+        model = CartridgeMinStock
+        fields = ['cartridge', 'department', 'min_stock']
+        widgets = {
+            'cartridge': forms.Select(attrs={'class': 'form-select'}),
+            'department': forms.Select(attrs={'class': 'form-select'}),
+            'min_stock': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
+
+
+class CartridgeInventoryForm(forms.ModelForm):
+    class Meta:
+        model = CartridgeInventory
+        fields = ['cartridge', 'current_stock']
+        widgets = {
+            'cartridge': forms.Select(attrs={'class': 'form-select'}),
+            'current_stock': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
---
+++ cartridges/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('department/add/', views.add_department, name='add_department'),
+    path('printer/add/', views.add_printer, name='add_printer'),
+    path('cartridge/add/', views.add_cartridge, name='add_cartridge'),
+    path('minstock/add/', views.set_min_stock, name='set_min_stock'),
+    path('currentstock/add/', views.set_current_stock, name='set_current_stock'),
+    path('order/', views.order, name='order'),
+    path('stats/', views.stats, name='stats'),
+    path('reference/', views.reference, name='reference'),
+    path('history/', views.history, name='history'),
+]
